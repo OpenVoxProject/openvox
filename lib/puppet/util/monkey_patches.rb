e79be53e4bdfd59da77dbcfea3c8fafdca1ac395
Romain Tartière <romain@blogreen.org>
2025-12-22 13:53:34 -1000

    Do not explicitly disable SSLv3
    SSLv3 is long gone, we could also probably also disable TLS 1.0 and 1.1,
    but [recent changes in the openssl gem freeze the default parameters](https://github.com/ruby/openssl/pull/925), and
    the default configuration seems to have the correct behavior on the
    systems I tested, refusing to connect to hosts with anything older than
    TLS 1.2.
    
    Drop this monkey patching to unbreak with version 4.0.0 of the openssl
    gem.
    
    Fixes https://github.com/OpenVoxProject/openbolt/issues/169

--- lib/puppet/util/monkey_patches.rb
+++ lib/puppet/util/monkey_patches.rb
@@ -52,12 +52,6 @@ end
 require_relative '../../puppet/ssl/openssl_loader'
 unless Puppet::Util::Platform.jruby_fips?
   class OpenSSL::SSL::SSLContext
-    if DEFAULT_PARAMS[:options]
-      DEFAULT_PARAMS[:options] |= OpenSSL::SSL::OP_NO_SSLv3
-    else
-      DEFAULT_PARAMS[:options] = OpenSSL::SSL::OP_NO_SSLv3
-    end
-
     alias __original_initialize initialize
     private :__original_initialize
 
