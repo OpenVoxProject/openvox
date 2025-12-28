92e7d97e77c2b78c0d3c7ba3eb0914edbf175c82
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-12-28 20:07:24 +0100

    Rewrite reference logic to use more expressive Ruby
    This uses fewer variables and is a bit shorter which arguably is making
    it easier to read.
    
    Signed-off-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- lib/puppet/reference/configuration.rb
+++ lib/puppet/reference/configuration.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
 config = Puppet::Util::Reference.newreference(:configuration, :depth => 1, :doc => "A reference for all settings") do
-  docs = {}
-  Puppet.settings.each do |name, object|
-    docs[name] = object
-  end
-
-  str = ''.dup
-  docs.sort { |a, b|
-    a[0].to_s <=> b[0].to_s
-  }.each do |name, object|
+  str = +''
+  Puppet.settings.sort_by { |name, _| name.to_s }.each do |name, object|
     # Make each name an anchor
     header = name.to_s
     str << markdown_header(header, 3)
@@ -23,33 +16,34 @@ config = Puppet::Util::Reference.newreference(:configuration, :depth => 1, :doc 
     str << "\n\n"
 
     # Now print the data about the item.
-    val = object.default
-    case name.to_s
-    when 'vardir'
-      val = 'Unix/Linux: /opt/puppetlabs/puppet/cache -- Windows: C:\ProgramData\PuppetLabs\puppet\cache -- Non-root user: ~/.puppetlabs/opt/puppet/cache'
-    when 'publicdir'
-      val = 'Unix/Linux: /opt/puppetlabs/puppet/public -- Windows: C:\ProgramData\PuppetLabs\puppet\public -- Non-root user: ~/.puppetlabs/opt/puppet/public'
-    when 'confdir'
-      val = 'Unix/Linux: /etc/puppetlabs/puppet -- Windows: C:\ProgramData\PuppetLabs\puppet\etc -- Non-root user: ~/.puppetlabs/etc/puppet'
-    when 'codedir'
-      val = 'Unix/Linux: /etc/puppetlabs/code -- Windows: C:\ProgramData\PuppetLabs\code -- Non-root user: ~/.puppetlabs/etc/code'
-    when 'rundir'
-      val = 'Unix/Linux: /var/run/puppetlabs -- Windows: C:\ProgramData\PuppetLabs\puppet\var\run -- Non-root user: ~/.puppetlabs/var/run'
-    when 'logdir'
-      val = 'Unix/Linux: /var/log/puppetlabs/puppet -- Windows: C:\ProgramData\PuppetLabs\puppet\var\log -- Non-root user: ~/.puppetlabs/var/log'
-    when 'hiera_config'
-      val = '$confdir/hiera.yaml. However, for backwards compatibility, if a file exists at $codedir/hiera.yaml, Puppet uses that instead.'
-    when 'certname'
-      val = "the Host's fully qualified domain name, as determined by Facter"
-    when 'hostname'
-      val = "(the system's fully qualified hostname)"
-    when 'domain'
-      val = "(the system's own domain)"
-    when 'srv_domain'
-      val = 'example.com'
-    when 'http_user_agent'
-      val = 'Puppet/<version> Ruby/<version> (<architecture>)'
-    end
+    val = case name.to_s
+          when 'vardir'
+            'Unix/Linux: /opt/puppetlabs/puppet/cache -- Windows: C:\ProgramData\PuppetLabs\puppet\cache -- Non-root user: ~/.puppetlabs/opt/puppet/cache'
+          when 'publicdir'
+            'Unix/Linux: /opt/puppetlabs/puppet/public -- Windows: C:\ProgramData\PuppetLabs\puppet\public -- Non-root user: ~/.puppetlabs/opt/puppet/public'
+          when 'confdir'
+            'Unix/Linux: /etc/puppetlabs/puppet -- Windows: C:\ProgramData\PuppetLabs\puppet\etc -- Non-root user: ~/.puppetlabs/etc/puppet'
+          when 'codedir'
+            'Unix/Linux: /etc/puppetlabs/code -- Windows: C:\ProgramData\PuppetLabs\code -- Non-root user: ~/.puppetlabs/etc/code'
+          when 'rundir'
+            'Unix/Linux: /var/run/puppetlabs -- Windows: C:\ProgramData\PuppetLabs\puppet\var\run -- Non-root user: ~/.puppetlabs/var/run'
+          when 'logdir'
+            'Unix/Linux: /var/log/puppetlabs/puppet -- Windows: C:\ProgramData\PuppetLabs\puppet\var\log -- Non-root user: ~/.puppetlabs/var/log'
+          when 'hiera_config'
+            '$confdir/hiera.yaml. However, for backwards compatibility, if a file exists at $codedir/hiera.yaml, Puppet uses that instead.'
+          when 'certname'
+            "the Host's fully qualified domain name, as determined by Facter"
+          when 'hostname'
+            "(the system's fully qualified hostname)"
+          when 'domain'
+            "(the system's own domain)"
+          when 'srv_domain'
+            'example.com'
+          when 'http_user_agent'
+            'Puppet/<version> Ruby/<version> (<architecture>)'
+          else
+            object.default
+          end
 
     # Leave out the section information; it was apparently confusing people.
     # str << "- **Section**: #{object.section}\n"
