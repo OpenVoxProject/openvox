086d1fd670704e1e34667e6edfc889f10fae10d8
Tim Meusel <tim@bastelfreak.de>
2026-02-26 11:03:10 +0100

    set version to 9.0.0

--- lib/puppet/version.rb
+++ lib/puppet/version.rb
@@ -8,7 +8,7 @@
 # Raketasks and such to set the version based on the output of `git describe`
 
 module Puppet
-  PUPPETVERSION = '8.25.0'
+  PUPPETVERSION = '9.0.0'
   IMPLEMENTATION = 'openvox'
 
   ##
