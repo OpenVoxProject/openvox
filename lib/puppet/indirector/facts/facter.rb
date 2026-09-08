b7893be0821c7c94c1095614ff68f0f50518f17a
Tim Meusel <tim@bastelfreak.de>
2026-09-08 16:25:05 +0200

    restructure debug logging
    Co-authored-by: Michael Harp <mike@mikeharp.com>
    Signed-off-by: Tim Meusel <tim@bastelfreak.de>

--- lib/puppet/indirector/facts/facter.rb
+++ lib/puppet/indirector/facts/facter.rb
@@ -67,11 +67,13 @@ class Puppet::Node::Facts::Facter < Puppet::Indirector::Code
     # Even though we no longer directly load facts in the terminus,
     # print out each .rb in the facts directory as module
     # developers may find that information useful for debugging purposes
-    if Puppet::Util::Log.sendlevel?(:info) && !dirs.empty?
+    unless dirs.empty?
       Puppet.info _("Loading facts")
-      dirs.each do |dir|
-        Dir.glob("#{dir}/*.rb").each do |file|
-          Puppet.debug { "Loading facts from #{file}" }
+      if Puppet::Util::Log.sendlevel?(:debug)
+        dirs.each do |dir|
+          Dir.glob("#{dir}/*.rb").each do |file|
+            Puppet.debug { "Loading facts from #{file}" }
+          end
         end
       end
     end
